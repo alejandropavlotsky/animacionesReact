acf5bfab6a3ce8b50ac91a09f6dd5f6f256d8c7d
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-02-07 13:52:34 +0100

    ADD animacion 4

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
 import Animacion1 from './componens/Animacion1';
 import Animacion2 from './componens/Animacion2';
 import Animacion3 from './componens/Animacion3';
+import Animacion4 from './componens/Animacion4';
 
 const App = () => {
   return (
@@ -17,7 +18,8 @@ const App = () => {
       <View style={styles.contenido}>
         {/* <Animacion1 /> */}
         {/* <Animacion2 /> */}
-        <Animacion3 />
+        {/* <Animacion3 /> */}
+        <Animacion4 />
       </View>
     </>
   );
